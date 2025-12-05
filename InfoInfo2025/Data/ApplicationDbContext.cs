c576e663b6b2f42c094302ad69970c9248d7892c
Hubert <201805967+reikanx@users.noreply.github.com>
2025-12-05 18:12:27 +0100

    Określa powiązania między tabelami w pliku ApplicationDbContext

--- InfoInfo2025/Data/ApplicationDbContext.cs
+++ InfoInfo2025/Data/ApplicationDbContext.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using InfoInfo2025.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace InfoInfo2025.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Text> Texts { get; set; }
+        public DbSet<Opinion> Opinions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Text>()
+                .HasOne(t => t.Category)
+                .WithMany(c => c.Texts)
+                .HasForeignKey(t => t.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Text>()
+                .HasOne(t => t.Author)
+                .WithMany(u => u.Texts)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Opinion>()
+                .HasOne(o => o.Text)
+                .WithMany(t => t.Opinions)
+                .HasForeignKey(o => o.TextId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Opinion>()
+                .HasOne(o => o.Author)
+                .WithMany(u => u.Opinions)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+
     }
 }
